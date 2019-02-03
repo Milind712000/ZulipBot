930119bde83ad684deb72a6f37bcacf16a3fbda0
Milind712000 <email2milindtopno.test@gmail.com>
2019-02-04 03:49:42 +0530

    scraping using bs4

---
+++ bs_script.py
@@ -0,0 +1,46 @@
+import re
+import datetime
+import pprint
+import requests as rs
+from bs4 import BeautifulSoup
+
+# helpers
+regex = re.compile(r'\d+\s\w+')
+
+def create_contest_obj(row):
+    cells = row.select("td")
+    contest_obj = {}
+    contest_obj['ID']=row.get("data-contestid")
+    contest_obj['Name']=cells[0].get_text().strip()
+    contest_obj['StartTime']= cells[2].get_text().strip()
+    contest_obj['duration']= cells[3].get_text().strip()
+    contest_obj['Time_left_Start']=regex.search(cells[4].get_text()).group()
+    contest_obj['Time_left_Registration']=regex.search(cells[5].get_text()).group()
+    return contest_obj
+
+# fetch contest page from codeforce
+cfContestUrl = 'https://codeforces.com/contests'
+try:
+    res = rs.get(cfContestUrl)
+    res.raise_for_status()
+except:
+    print("failed to get the page at ",cfContestUrl)
+
+soup = BeautifulSoup(res.text,"html.parser")
+
+# extract contest details
+
+#array with current and upcoming contests
+contests = []
+contestSoup = soup.select_one(".datatable table").select("tr")[1:]
+for tr in contestSoup:
+    obj = create_contest_obj(tr)
+    contests.append(obj)
+
+# save to a file
+time = datetime.datetime.now()
+scraping_time = time.strftime('%m-%d-%Y_%I:%M%p')
+name = 'contests_%s_.json'%scraping_time
+fh = open(name,'w')
+fh.write(pprint.pformat(contests))
+fh.close()
